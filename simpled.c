91e9440d614e38423d620ae204a60578c23e039b
David Mercado <David@Davids-MacBook-Pro-5.local>
2017-04-09 18:19:24 -0400

    updated simpled

--- simpled.c
+++ simpled.c
@@ -8,33 +8,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>     //for atoi
-#include "csapp.h"
+#include "csapp.h"      //for sockets and what not
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~ssSet~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int simpleSet(char *MachineName, int TCPport, int SecretKey, char *variableName, char *value, int dataLength)
+{
+    return 0;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~ssGet~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 int main(int argc, const char * argv[])
 {
-    const int Port = atoi(argv[1]);                                             //converts port to int (MIGHT CHANGE TO LONG)!!!!
-    const unsigned long SecretKey = atoi(argv[2]);                              //converts SecretKey to unsigned long
-    const unsigned long limit = 4294967296;                                     //(2^32-1) limit for SecretKey
-    
-    if(argc != 3)                                                         //checks for correct parameters
+    const int LISTEN_PORT = atoi(argv[1]);                                      //converts port to int (MIGHT CHANGE TO LONG)!!!!
+    const unsigned long SECRET_KEY = atoi(argv[2]);                             //converts SecretKey to unsigned long
+    const unsigned long LIMIT = 4294967296;                                     //(2^32-1) limit for SecretKey
+    const int ROW = 50;                                                         
+    const int COLUMN = 2;
+
+    if(argc != 3)                                                               //checks for correct parameters
     {
         fprintf(stderr, "Incorrect number of parameters: <port> %s <SecretKey> %s \n ", argv[1], argv[2]);
         exit(0);
     }
-    else if ((Port <= 0) || (SecretKey <= 0) || (SecretKey >= limit))
+    else if ((LISTEN_PORT <= 0) || (SECRET_KEY <= 0) || (SECRET_KEY >= LIMIT))
     {
-        fprintf(stderr, "Incorrect Port or SecretKey Range!!: <port> %d <SecretKey> %ld \n ", Port, SecretKey);
+        fprintf(stderr, "Incorrect Port or SecretKey Range!!: <port> %d <SecretKey> %ld \n ", LISTEN_PORT, SECRET_KEY);
         exit(0);
     }
     
+    char *Record[ROW][COLUMN];                                                  //Initalized 2 deminsional array to hold variables
     int listenfd;                                                               //for listening socket
-    int connfd;
+    int connfd;                                                                 //for accept
     struct sockaddr_in clientAddr;                                              //socket address structure for the internet
     struct hostent *clientHostEntry;                                            //used to represent an entry in the hosts database
     char *clientIP;                                                             //for clients IP address
     unsigned short clientPort;                                                  //for clients port number
     
-    listenfd = open_listenfd(Port);                                             //open and return a listening socket on port (bind)
+    listenfd = open_listenfd(LISTEN_PORT);                                      //open and return a listening socket on port (bind)
     socklen_t addrLength = sizeof(clientAddr);                                  //holds the length of the clients address
 
     while(1)                                                                    //continously listening for clients
@@ -45,32 +59,19 @@ int main(int argc, const char * argv[])
             perror("accept error");
         }
         
-        char * Request;
-        char * Detail;
-        char * Status;
+        unsigned long Password;
+        char * Request;                                                         //store Request type
+        char * Detail;                                                          //store Details
+        char * Status;                                                          //store if its successful or not
         
-        fprintf(stdout, "Secret Key = %ld \n", SecretKey);
-        fprintf(stdout, "Request type = %s \n", Request);
-        fprintf(stdout, "Detail = %s \n", Detail);
+        fprintf(stdout, "Secret Key = %ld \n", Password);                       //displays SecretKey provided by client
+        fprintf(stdout, "Request type = %s \n", Request);                       //displays Request type provided by client
+        fprintf(stdout, "Detail = %s \n", Detail);                              //displays Details provided by client
         fprintf(stdout, "Completion = %s \n", Status);
         fprintf(stdout, "-------------------------- \n");
         
-        
+        fprintf(stdout, "Port number %d  SecretKey %ld \n", LISTEN_PORT ,SECRET_KEY);       //DELETE!!!!!!!!!!!!!!!!!!!!
+
     }
-    
-    
-    
-    
-    
-    fprintf(stdout, "Port number %d  SecretKey %ld \n", Port ,SecretKey);       //DELETE!!!!!!!!!!!!!!!!!!!!
-
-    
-    
-    
-    
-    
-    
-    
-
     return 0;
 }
