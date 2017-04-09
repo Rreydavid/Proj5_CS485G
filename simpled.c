f8c3e367a47455a6854e06df0c00492ee98b02e9
David Mercado <David@Davids-MacBook-Pro-5.local>
2017-04-09 17:35:35 -0400

    added listen and accept

--- simpled.c
+++ simpled.c
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
 {
     const int Port = atoi(argv[1]);                                             //converts port to int (MIGHT CHANGE TO LONG)!!!!
     const unsigned long SecretKey = atoi(argv[2]);                              //converts SecretKey to unsigned long
-    const unsigned long limit = 4294967296;
+    const unsigned long limit = 4294967296;                                     //(2^32-1) limit for SecretKey
     
     if(argc != 3)                                                         //checks for correct parameters
     {
@@ -26,12 +26,37 @@ int main(int argc, const char * argv[])
         fprintf(stderr, "Incorrect Port or SecretKey Range!!: <port> %d <SecretKey> %ld \n ", Port, SecretKey);
         exit(0);
     }
-    struct sockaddr_in clientAddr;
-    struct hostent *clientHostEntry;
-    char *clientIP;
-    unsigned short clientPort;
     
+    int listenfd;                                                               //for listening socket
+    int connfd;
+    struct sockaddr_in clientAddr;                                              //socket address structure for the internet
+    struct hostent *clientHostEntry;                                            //used to represent an entry in the hosts database
+    char *clientIP;                                                             //for clients IP address
+    unsigned short clientPort;                                                  //for clients port number
     
+    listenfd = open_listenfd(Port);                                             //open and return a listening socket on port (bind)
+    socklen_t addrLength = sizeof(clientAddr);                                  //holds the length of the clients address
+
+    while(1)                                                                    //continously listening for clients
+    {
+        connfd = Accept(listenfd, (SA *)&clientAddr, &addrLength);              //accepts and creates a file descriptor for this connection
+        if(connfd < 1)
+        {
+            perror("accept error");
+        }
+        
+        char * Request;
+        char * Detail;
+        char * Status;
+        
+        fprintf(stdout, "Secret Key = %ld \n", SecretKey);
+        fprintf(stdout, "Request type = %s \n", Request);
+        fprintf(stdout, "Detail = %s \n", Detail);
+        fprintf(stdout, "Completion = %s \n", Status);
+        fprintf(stdout, "-------------------------- \n");
+        
+        
+    }
     
     
     
