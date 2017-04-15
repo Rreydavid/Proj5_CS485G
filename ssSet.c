c1add5e3b2bee4dd6960248e63c9d83cf70722c3
David Mercado <David@Davids-MacBook-Pro-5.local>
2017-04-15 17:24:11 -0400

    updated set

--- ssSet.c
+++ ssSet.c
@@ -7,49 +7,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "csapp.h"
+#include <arpa/inet.h>      //for network ordering
 
 int main(int argc, char *argv[])
 {
-	// Declare Variables
-	int toserverfd;
-	int port;
+    const int MAX_REQUEST_BUFF = 128;                                           // Declare Variables
 	char *host;
-	char *name[MAXLINE];
-	char *value[MAXLINE];
+    int port;
+    unsigned long SecretKey;
+    unsigned long SecretKeyNet;
+    char *SetRequest[MAX_REQUEST_BUFF] = {NULL};
+    char *VariableName[MAXLINE];                                                //DELETE!!!!!!!!!!
+	char *VariableValue[MAXLINE];                                               //DELETE!!!!!!!!!!
+    int toserverfd;
 	rio_t rio;
-
-	// Check for proper amount of command line arguments
-	if(argc != 6)
+    
+//------------------------------------------------------------------------------
+	if(argc != 6)                                                               // Check for proper amount of command line arguments
 	{
 		// If not 6 command line arguments, output to stderr with usage and values that were input.
         fprintf(stderr, "usage: <MachineName> %s, <TCPport> %s, <SecurityKey> %s, <VariableName> %s, <Value> %s \n ", argv[1], argv[2], argv[3], argv[4], argv[5]);
         exit(0);
 	}
-	else if(sizeof(argv[5]) > 100)
+    else if(sizeof(argv[5]) > 100)                                              //If Variable value exceeds 100 bytes
 	{
 		fprintf(stderr, "The value to be set must not exceed 100 bytes.");
 		exit(0);
 	}
-
-	// Update the host and port variables with their values from ARGV
-	host = argv[1];
+    
+//------------------------------------------------------------------------------
+	host = argv[1];                                                             // set argv values into usable variables
 	port = atoi(argv[2]);
-	*name = argv[4];
-	*value = argv[5];
-
-	
-    toserverfd = Open_clientfd(host, port);       //Create file descriptor to the server, using wrapper
-
+    SecretKey = atoi(argv[3]);                                                  //converts string secret key to unsigned long
+    SecretKeyNet = htonl(SecretKey);                                            //converts secretkey to network order
+    
+    
+    *VariableName = argv[4];
+	*VariableValue = argv[5];
+    
+    
+    SetRequest[0] = argv[0];                                //Bytes 0-3: A 4-byte unsigned integer containing SecretKey in network byte order
+    SetRequest[4] = 0;                                      //Byte 4: A 1-byte unsigned integer containing the type of request: set (0)
+    SetRequest[8] = argv[4];                                //Bytes 8-23: a null-terminated variable name, no longer than 15 characters.
+    SetRequest[24] = argv[0];                        //Bytes 24-27: A 4-byte unsigned integer (in network order) giving the length of the value
+    SetRequest[28] = argv[5];           //Bytes 28 ..: The value itself. The client need not send any more than the number of bytes required.
+    
+    
+    toserverfd = Open_clientfd(host, port);                                     //Create file descriptor to the server, using wrapper
 	// Ready for I/O
-	Rio_readinitb(&rio, toserverfd);
+    Rio_readinitb(&rio, toserverfd);                                            //Associate a toserverfd with a read buffer and reset buffer
 
 	// Send to the server
-	Rio_writen(toserverfd, *name, strlen(*name));
-	Rio_writen(toserverfd, *value, strlen(*value));
+    Rio_writen(toserverfd, *argv[3], strlen(argv[3]));
+
+	Rio_writen(toserverfd, *VariableName, strlen(*VariableName));
+	Rio_writen(toserverfd, *VariableValue, strlen(*VariableValue));
 
 	// From the server
-	Rio_readlineb(&rio, *name, MAXLINE);
-	Fputs(*name, stdout);
+    Rio_readnb(&rio, *VariableName, 1);                                //reads buffer from sever
+	Fputs(*VariableName, stdout);
 
 	Close(toserverfd);
 	exit(0);
