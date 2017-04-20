7fd3e5ce82e04bbc32d795120dfec74da644fb17
David Mercado <David@Davids-MacBook-Pro-5.local>
2017-04-20 18:10:11 -0400

    ssGet

--- ssGet.c
+++ ssGet.c
@@ -1,8 +1,7 @@
 //  David Mercado, Patrick True
 //  CS485G Section 2
-//  Project 5
+//  Project 5 - ssGet.c
 //  21 April 201
-//  ssGet Client
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,62 +15,56 @@ int main(int argc, char *argv[])
 	int ServerRequest1[2];
 	char *host;
 	int port;
-	int ReturnedValue;
 	char *buffer[100] = {NULL};
 	char *ServerRequest2[15] = {NULL};
-	int caughtsize[1];
-	int ValueReturnLength;
+	int ReturnedStatusNet[1];
+    unsigned int caughtsize[1];
+    int ReturnedStatus;
+	unsigned int ValueReturnLength;
 	unsigned long SecretKey;
 	rio_t rio;
-
-	// Check for proper amount of command line arguments
-	if(argc != 5)
+    
+//------------------------------------------------------------------------------
+	if(argc != 5)                                                               // Check for proper amount of command line arguments
 	{
 		// If not 5 command line arguments, output to stderr with usage and values that were input.
-        fprintf(stderr, "usage: <MachineName> %s, <TCPport> %s, <SecurityKey> %s, <VariableName> %s \n ", argv[1], argv[2], argv[3], argv[4]);
+        fprintf(stderr, "usage: <MachineName> <TCPport> <SecurityKey> <VariableName>");
         exit(0);
 	}
-
-	// Assign the values into host and port
-	host = argv[1];
+//------------------------------------------------------------------------------
+	
+	host = argv[1];                                                             // Assign the values into host and port
 	port = atoi(argv[2]);
 
 	// Build the first part of the server request with the security key and the request type
-	SecretKey = atoi(argv[3]);														//converts string secret key to unsigned long
-    ServerRequest1[0] = htonl(SecretKey);                                           //converts secretkey to network order
-	ServerRequest1[1] = 1;
+	SecretKey = atoi(argv[3]);                                                  //converts string secret key to unsigned long
+    ServerRequest1[0] = htonl(SecretKey);                                       //converts secretkey to network order
+    ServerRequest1[1] = 1;                                                      //type request ssGet
+	
+	memcpy(ServerRequest2, argv[4], 14);                                        // Build the second part of the server request with the variable name
+	
+	toserverfd = open_clientfd(host, port);                                     // Create file descriptor to the server
 
-	// Build the second part of the server request with the variable name
-	memcpy(ServerRequest2, argv[4], 14);
-
-	// Create file descriptor to the server
-	toserverfd = open_clientfd(host, port);
-
-	// Ready for I/O
-	Rio_readinitb(&rio, toserverfd);
+	Rio_readinitb(&rio, toserverfd);                                            // Ready for I/O
 	 
-	// Send request to the server
-	Rio_writen(toserverfd, ServerRequest1, sizeof(ServerRequest1));
+	Rio_writen(toserverfd, ServerRequest1, sizeof(ServerRequest1));             // Send request to the server
 	Rio_writen(toserverfd, ServerRequest2, sizeof(ServerRequest2));
 
-	// from the server
-	// Catch the success or failure byte, 3 trash bytes
-	rio_readnb(&rio, (void*)&ReturnedValue, 4);
+    Rio_readnb(&rio,(void*)&ReturnedStatusNet, 4);                              //Sucess/ailure from the server
+    ReturnedStatus = ntohl(ReturnedStatusNet[0]);                               //converts the returne success/fail value from net to host order
 
-	if(ReturnedValue == -1)
+    if(ReturnedStatus == -1)                                                    //if fails output message
 	{
 		printf("Failure.");
 	}
-
-	// Catch length of the value being returned
-	Rio_readnb(&rio, (void *)&caughtsize, 4);
-	// Network to Host order the returned length 
-	ValueReturnLength = ntohl(caughtsize[0]);
-
-	// Catch the actual returned value requested
-	Rio_readnb(&rio, buffer, ValueReturnLength);
-	printf("%s\n", buffer);	
-	
-	Close(toserverfd);
+    else
+    {
+        Rio_readnb(&rio, (void *)&caughtsize, 4);                                   // Catch length of the value being returned
+        ValueReturnLength = ntohl(caughtsize[0]);                                   // Network to Host order the returned length
+        Rio_readnb(&rio, buffer, ValueReturnLength);                                // Catch the actual returned value requested
+        printf("%s\n", buffer);
+    }
+    
+    Close(toserverfd);                                                          //close connection to server
 	exit(0);
 }
